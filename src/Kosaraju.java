2a1ebbe04cbe3230f61120f395c98896ac8ae6bd
fab987654321 <fab987654321>
2022-10-18 13:57:24 +0200

    premier parcours

--- src/Kosaraju.java
+++ src/Kosaraju.java
@@ -1,81 +1,110 @@
 import java.util.HashSet;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.Stack;
 
 public class Kosaraju {
 
-    private Graph<String> leGraph ;
-    public  Kosaraju( Graph<String> graph){
+    private Graph<String> leGraph;
+
+    public Kosaraju(Graph<String> graph) {
         this.leGraph = graph;
     }
 
-  public int[] sccs(){
-          try {
+    /**
+     * @return
+     * @throws Exception
+     * @throws NumberFormatException
+     */
+    public int[] sccs() throws NumberFormatException, Exception {
 
-           // System.out.println( leGraph.getListArc());
-            
-          //leGraph.getAdj(0);
-      } catch (Exception e) {
-         // System.out.println(e);
-      }
+        Set<Integer> visite = new HashSet<>();
+        // Pile
+        Stack<Integer> sortie = new Stack<>();
+        Stack<Integer> chemin = new Stack<>();
 
-       
-      Set<Integer> visite = new HashSet<>();
-      //Pile
-      Stack<Integer> sortie = new Stack<>();
-      Stack<Integer> chemin = new Stack<>(); 
+        // Variable tampon
+        List<Integer> tmpAdj = null;
+        int intIsVisite = -1;
+        int tmpSommetActuel = -1;
 
-      //Je pars d'un point de départ (ligne 1)
-      chemin.add(leGraph.getAdj(1).get(0));
-      //Mets dans la pile des visitées
-      visite.add(leGraph.getAdj(1).get(0));
-      
-      for (int i = 0; i < leGraph.order();i++) {
-        //Int en haut de la pile
-        chemin.lastElement();
-        //List<int> des adjacents
-        leGraph.getAdj(chemin.lastElement());
-        
-        //recup list adj
-        //Si pas adjacent non visité alors
-                //ajoute au stack sortie 
-                //recommence la boucle
-        //prendre element à la position 1
-        //Ajoute le dis élément à la pile visité 
-        //Ajoute le dis élément au chemin
-            //Si pas adjacent non visité alors
-                //ajoute au stack sortie 
-                //recommence la boucle
-      }
+        // Pars d'un sommet src (ligne 0)
+        chemin.add(Integer.parseInt(leGraph.getIncidency(0)[0]));
+        visite.add(chemin.get(0));
 
-      //Boucle
-        //Regarde les sommets accécibles
-            //Si exite sommet accessible non visité
-                //Avance au dis sommet
-            //Sinon
-                //Mets dans la pile des sommets fini (noir)
-                //Recule au sommet précedent
+        // TODO transformer en while sur la pile chemin et/ou sortie
+        // Boucle sur tt les arc
+        int i = 0;
+        while (true) {
+            i++;
+            try {
+                System.out.println("---------------");
+                // Sommet actuel
+                tmpSommetActuel = chemin.lastElement();
+                // List adj du sommet
+                tmpAdj = leGraph.getAdj(tmpSommetActuel);
+
+                System.out.println(tmpSommetActuel + ";" + tmpAdj);
+                intIsVisite = -1;
+
+                // Si au moins 1 adjacent
+                if (tmpAdj.size() > 0)
+                    for (int each : tmpAdj) {
+                        // Si au moins 1 adjacent non visité
+                        if (!visite.contains(each)) {
+                            System.out.println(tmpSommetActuel + " visite " + each);
+                            intIsVisite = each;
+                            // Quite le for (normalement)
+                            break;
+                        }
+                    }
+                System.out.println("each " + intIsVisite);
+                // Si Sommet accéssible
+                if (intIsVisite != -1) {
+                    // Ajoute l'élément à la pile visité et au chemin
+                    chemin.add(intIsVisite);
+                    visite.add(intIsVisite);
+                }
+                // Cas où aucun adj n'est dispo
+                else {
+                    System.out.println("Dépilage de " + tmpSommetActuel);
+                    // Dépile le chemin et empile dans sortie
+                    sortie.add(chemin.pop());
+                }
+
+            } catch (NoSuchElementException e) {
+                // Cas ou on a tout dépilé
+                System.out.println("Fini de dépiler" + sortie.toString());
+                break;//
+            }
+        }
+        // Boucle
+        // Regarde les sommets accécibles
+        // Si exite sommet accessible non visité
+        // Avance au dis sommet
+        // Sinon
+        // Mets dans la pile des sommets fini (noir)
+        // Recule au sommet précedent
 
         return new int[10];
-  }
+    }
 
-    //TODO
-    //"Parcour en profondeur"
-    String[] DFS(Graph unGraph){
+    // TODO
+    // "Parcour en profondeur"
+    String[] DFS(Graph unGraph) {
 
-
-        //Retourne un tableau contennant les composantes fortement connexe
+        // Retourne un tableau contennant les composantes fortement connexe
         return new String[2];
     }
 
-    Stack SortTime(){
-
+    Stack SortTime() {
 
         return new Stack();
     }
-    //1 Sortime
-    //2 Transpose
-    //3 DFS par rapport au temps de sortie (sur la transposé)
+    // 1 Sortime
+    // 2 Transpose
+    // 3 DFS par rapport au temps de sortie (sur la transposé)
 
-        //Avec empilage et génération de "foret d'arborescence"
-}//Class
+    // Avec empilage et génération de "foret d'arborescence"
+}// Class
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String filename = "formulas/testSet0/formula1.txt";
-        //String filename = "formulas/formula.txt";
+        String filename = "formulas/testSet0/formula2.txt";
+        // String filename = "formulas/formula.txt";
         if (0 < args.length) {
             filename = args[0];
         }
@@ -16,18 +16,23 @@ public class Main {
         System.out.println(graph);
 
         Kosaraju k = new Kosaraju(graph);
-        int[] composantes = k.sccs();
-
-        //System.out.println((graph.getTranspose()).toString());
-/*
-        if (TwoSat.checkConsistency(composantes)) {
-            System.out.println("Formula " + filename + ": satisfiable");
-            exit(0);
-        } else {
-            System.out.println("Formula " + filename + ": unsatisfiable");
-            exit(-1);
+        try {
+            int[] composantes = k.sccs();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-  */
+
+        // System.out.println((graph.getTranspose()).toString());
+        /*
+         * if (TwoSat.checkConsistency(composantes)) {
+         * System.out.println("Formula " + filename + ": satisfiable");
+         * exit(0);
+         * } else {
+         * System.out.println("Formula " + filename + ": unsatisfiable");
+         * exit(-1);
+         * }
+         */
         System.out.println("Fin");
         exit(0);
 
