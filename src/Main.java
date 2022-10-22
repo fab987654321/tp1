85fd50b07dca7445c37cd84b6a672b457c5250af
fab987654321 <fab987654321>
2022-10-22 12:43:56 +0200

    nom + prenom

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,21 @@ import static java.lang.System.exit;
 public class Main {
 
     public static void main(String[] args) {
+        // TODO mettre ton nom de famille
+        System.out.println("Code réalisé par Fabien AUTARD et Bryce");
 
         String filename = "formulas/testSet0/formula0.txt"; // non ok S
-        //String filename = "formulas/testSet0/formula1.txt";// non ok S
-        //String filename = "formulas/testSet0/formula2.txt";//OK I
-        //String filename = "formulas/testSet0/formula3.txt";//non OK S
-        //String filename = "formulas/testSet0/formula4.txt";//ok S
-        //String filename = "formulas/testSet0/formula5.txt";//ok S
-        //String filename = "formulas/testSet0/formula6.txt";//ok S
-        //String filename = "formulas/testSet0/formula7.txt";//ok S
-        //String filename = "formulas/testSet0/formula8.txt";//non ok I
-        //String filename = "formulas/testSet0/formula9.txt";//ok S
+        // String filename = "formulas/testSet0/formula1.txt";// non ok S
+        // String filename = "formulas/testSet0/formula2.txt";//OK I
+        // String filename = "formulas/testSet0/formula3.txt";//non OK S
+        // String filename = "formulas/testSet0/formula4.txt";//ok S
+        // String filename = "formulas/testSet0/formula5.txt";//ok S
+        // String filename = "formulas/testSet0/formula6.txt";//ok S
+        // String filename = "formulas/testSet0/formula7.txt";//ok S
+        // String filename = "formulas/testSet0/formula8.txt";//non ok I
+        // String filename = "formulas/testSet0/formula9.txt";//ok S
 
-        //String filename = "formulas/testSet1/formula8.txt";
+        // String filename = "formulas/testSet1/formula8.txt";
         // String filename = "formulas/formule-2-sat.txt";
         if (0 < args.length) {
             filename = args[0];
